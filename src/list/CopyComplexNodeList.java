816acc3cb4925adaa57a52ad72c427180f9553a3
AlanZhangAoYu <z7954221907482@163.com>
2022-02-04 17:37:47 +0800

    链表练习2

---
+++ src/list/CopyComplexNodeList.java
@@ -0,0 +1,18 @@
+package list;
+
+/**
+ * @author ZAY
+ * 复制含有随机指针节点的链表
+ * class Node{
+ *     int data;
+ *     Node next;
+ *     Node random;
+ *     public Node(int data){
+ *         this.data=data;
+ *     }
+ * }
+ * random指针是单链表中新增的指针，可能指向链表中的任意一个节点，也可能为空; 给定一个这样的链表head，实现一个函数完成这个链表的复制，
+ * 并返回新链表的头结点
+ */
+public class CopyComplexNodeList {
+}
---
+++ src/list/SingleListPartition.java
@@ -0,0 +1,9 @@
+package list;
+
+/**
+ * @author ZAY
+ * 单链表给定一个数num，小于num的数放左边，等于num的数放中间，大于num的数放右边，且每个数之间顺序保持一致(内存连续的数组做不到，但单链表可以)
+ * 类似于快速排序中的 partition 操作
+ */
+public class SingleListPartition {
+}
