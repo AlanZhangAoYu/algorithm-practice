fd22c11405b7cea8c0c319d0c0a5576fbac9f28e
AlanZhangAoYu <z7954221907482@163.com>
2022-02-07 21:48:07 +0800

    链表练习3

--- src/list/SingleListPalindrome.java
+++ src/list/SingleListPalindrome.java
@@ -8,7 +8,7 @@ import list.util.SingleNode;
  * 判断一个单链表是不是回文 1,2,3,2,1
  * 另外增加快慢指针练习
  * 奇数链表: 快指针走完，慢指针在中点; 快指针走完，慢指针在中点前一个; 快指针走完，慢指针在中点后一个
- * 偶数链表: 快指针走完，慢指针指向中间两个中的前一个; 快指针走完，慢指针在中间两个中的后一个;
+ * 偶数链表: 快指针走完，慢指针指向中间两个中的前一个; 快指针走完，慢指针指向中间两个中的后一个;
  *          快指针走完，慢指针在中间两个的前面; 快指针走完，慢指针在中间两个的后面
  */
 public class SingleListPalindrome {
@@ -17,6 +17,7 @@ public class SingleListPalindrome {
             return false;
         }
         SingleNode p=head.getNext();
+        //用一个栈来存储链表前半截,再将链表后半截与出栈的数相比较，只要有一个不相等，就不是回文链表; 若最后栈弹空，是回文链表
         SingleNode stack=new SingleNode(0);
         for(int i=1;i<=SingleListUtils.getLength(head) / 2;i++){
             SingleListUtils.addLast(stack,p.getData());
@@ -35,17 +36,60 @@ public class SingleListPalindrome {
         return true;
     }
     public static void main(String[] args){
+        System.out.println("===偶数长度的链表=====");
         SingleNode head1=new SingleNode(0);
         SingleListUtils.addLast(head1,1);
         SingleListUtils.addLast(head1,2);
         SingleListUtils.addLast(head1,3);
         SingleListUtils.addLast(head1,4);
-        SingleListUtils.addLast(head1,4);
-        SingleListUtils.addLast(head1,3);
+        SingleListUtils.addLast(head1,5);
+        SingleListUtils.addLast(head1,6);
         SingleListUtils.addLast(head1,2);
         SingleListUtils.addLast(head1,1);
         SingleListUtils.printList(head1);
         System.out.println(isPalindrome(head1));
+
+        //快指针走完，慢指针指向中间两个中的前一个
+        SingleNode fast=head1.getNext();
+        SingleNode slow =head1;
+        while(fast != null){
+            fast=fast.getNext();
+            fast=fast.getNext();
+            slow = slow.getNext();
+        }
+        System.out.println("slow:"+ slow.getData());
+
+        //快指针走完，慢指针指向中间两个中的后一个
+        fast=head1.getNext();
+        slow =head1.getNext();
+        while(fast != null){
+            fast=fast.getNext();
+            fast=fast.getNext();
+            slow = slow.getNext();
+        }
+        System.out.println("slow:"+ slow.getData());
+
+        //快指针走完，慢指针在中间两个的前面
+        fast=head1.getNext();
+        slow =head1;
+        while(fast != null){
+            fast=fast.getNext();
+            fast=fast.getNext();
+            slow = slow.getNext();
+        }
+        System.out.println("(一直有错,不会)slow:"+ slow.getData());
+
+        //快指针走完，慢指针在中间两个的后面
+        fast=head1.getNext();
+        slow =head1.getNext().getNext();
+        while(fast != null){
+            fast=fast.getNext();
+            fast=fast.getNext();
+            slow = slow.getNext();
+        }
+        System.out.println("slow:"+ slow.getData());
+
+        System.out.println("===奇数长度的链表====");
         SingleNode head2=new SingleNode(0);
         SingleListUtils.addLast(head2,1);
         SingleListUtils.addLast(head2,2);
@@ -56,5 +100,35 @@ public class SingleListPalindrome {
         SingleListUtils.addLast(head2,2);
         SingleListUtils.printList(head2);
         System.out.println(isPalindrome(head2));
+
+        //快指针走完，慢指针在中点
+        fast=head2.getNext();
+        slow =head2.getNext();
+        while(fast.getNext() != null){
+            fast=fast.getNext();
+            fast=fast.getNext();
+            slow = slow.getNext();
+        }
+        System.out.println("slow:"+ slow.getData());
+
+        //快指针走完，慢指针在中点前一个
+        fast=head2.getNext();
+        slow =head2;
+        while(fast.getNext() != null){
+            fast=fast.getNext();
+            fast=fast.getNext();
+            slow = slow.getNext();
+        }
+        System.out.println("slow:"+ slow.getData());
+
+        //快指针走完，慢指针在中点后一个
+        fast=head2.getNext();
+        slow =head2.getNext().getNext();
+        while(fast.getNext() != null){
+            fast=fast.getNext();
+            fast=fast.getNext();
+            slow = slow.getNext();
+        }
+        System.out.println("slow:"+ slow.getData());
     }
 }
--- src/list/SingleListPartition.java
+++ src/list/SingleListPartition.java
@@ -1,9 +1,79 @@
 package list;
 
+import list.util.SingleListUtils;
+import list.util.SingleNode;
+
 /**
  * @author ZAY
  * 单链表给定一个数num，小于num的数放左边，等于num的数放中间，大于num的数放右边，且每个数之间顺序保持一致(内存连续的数组做不到，但单链表可以)
- * 类似于快速排序中的 partition 操作
+ * 常规方法: 将链表中的数复制到一个内存连续的数组中，在执行类似于快速排序中的 partition 操作，最后将处理好的数组复制回原链表，原有顺序不一定一致
  */
 public class SingleListPartition {
+    public static SingleNode partition(SingleNode head,int num){
+        //类似于 "桶" 的思想，三个"桶"分别装大于等于小于num的数，最后在串到一起
+        SingleNode low=new SingleNode(0);
+        SingleNode equals=new SingleNode(0);
+        SingleNode high=new SingleNode(0);
+        SingleNode p=head.getNext();
+        while (p != null){
+            if(p.getData() < num){
+                SingleListUtils.addLast(low,p.getData());
+            }
+            if(p.getData() == num){
+                SingleListUtils.addLast(equals,p.getData());
+            }
+            if(p.getData() > num){
+                SingleListUtils.addLast(high,p.getData());
+            }
+            p=p.getNext();
+        }
+        System.out.print("low:");
+        SingleListUtils.printList(low);
+        System.out.print("equals:");
+        SingleListUtils.printList(equals);
+        System.out.print("high:");
+        SingleListUtils.printList(high);
+        p=low;
+        if(low.getNext() != null){
+            p=low.getNext();
+            while (p.getNext() != null){
+                p=p.getNext();
+            }
+        }
+        if(equals.getNext() != null){
+            p.setNext(equals.getNext());
+            while (p.getNext() != null){
+                p=p.getNext();
+            }
+        }
+        if(high.getNext() != null){
+            p.setNext(high.getNext());
+        }
+        if(low.getNext() != null){
+            return low;
+        }
+        if(equals.getNext() != null){
+            return equals;
+        }
+        if(high.getNext() != null){
+            return high;
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        SingleNode head=new SingleNode(0);
+        SingleListUtils.addLast(head,81);
+        SingleListUtils.addLast(head,5);
+        SingleListUtils.addLast(head,45);
+        SingleListUtils.addLast(head,16);
+        SingleListUtils.addLast(head,34);
+        SingleListUtils.addLast(head,71);
+        SingleListUtils.addLast(head,55);
+        SingleListUtils.addLast(head,69);
+        SingleListUtils.addLast(head,99);
+        SingleListUtils.addLast(head,27);
+        SingleListUtils.printList(head);
+        SingleNode result=partition(head,50);
+        SingleListUtils.printList(result);
+    }
 }
