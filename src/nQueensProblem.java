83350f0a09c914dc852d81d77a1de6079b101e9f
鱼鱼雨雨玉玉 <z7954221907482@163.com>
2022-03-08 09:31:15 +0000

    N皇后0.6

--- src/nQueensProblem.java
+++ src/nQueensProblem.java
@@ -1,4 +1,3 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -6,8 +5,9 @@ import java.util.Scanner;
  * N皇后问题
  * 在 N*N 的棋盘上要摆放N个皇后，要求任意两个皇后不同行，不同列，也不在同一个斜线上
  * 给定一个整数N,返回N皇后的摆法有几种
+ * 参考: https://blog.csdn.net/piyongduo3393/article/details/86497081
  */
-public class nQueensProblem {
+public class NQueueProblem {
     public static void initCheckerboard(int[][] checkerboard){
         //初始化棋盘，没有棋子时为0，有棋子时为1
         if(checkerboard == null){
@@ -34,60 +34,34 @@ public class nQueensProblem {
     public static boolean canPut(int[][] checkerboard,int[] location,int n){
         //判断在当前传入的位置location处是否可以放棋子
         for(int i=0;i<n;i++){
-            if(checkerboard[location[0]][i] == 1){
-                return false;
-            }
-            if(checkerboard[i][location[1]] == 1){
-                return false;
-            }
-        }
-        //以下代码用来判断斜边，flag用来记录从当前标签出发向上或向下走了几步
-        /*int flag=0;
-        while (location[0]+flag >=0 && location[0]+flag < n && location[1]+flag >=0 && location[1]+flag < n){
-            if(checkerboard[location[0]+flag][location[1]+flag] == 1){
-                return false;
-            }
-            flag=flag-1;
-        }
-        flag=0;
-        while (location[0]+flag >=0 && location[0]+flag < n && location[1]+flag >=0 && location[1]+flag < n){
-            if(checkerboard[location[0]+flag][location[1]+flag] == 1){
-                return false;
-            }
-            flag=flag+1;
-        }*/
-        return true;
-    }
-    public static int process(int[][] checkerboard,int n){
-        //list用来记录一次成功的放入中所有棋子的位置
-        if(checkerboard == null){
-            return -0xffff;
-        }
-        LinkedList<int[]> list=new LinkedList<>();
-        //flag为棋盘第一行每次在列上变动的指针
-        int result=0,flag;
-        int[] location={0,0};
-        for(flag=0;flag< checkerboard[0].length;flag++){
-            checkerboard[0][flag]=1;
-            for(int i=1;i<checkerboard.length;i++){
-                location[0]=i;
-                for(int j=0;j<checkerboard[0].length;j++){
-                    location[1]=j;
-                    if(canPut(checkerboard, location, n)) {
-                        checkerboard[i][j]=1;
-                        list.add(location);
-                        print(checkerboard);
+            for(int j=0;j<n;j++) {
+                if(checkerboard[i][location[1]]==1 || checkerboard[location[0]][j] == 1) {
+                    return false;
+                }
+                if(Math.abs(i-location[0])==Math.abs(j-location[1])){
+                    if(checkerboard[i][j]==1){
+                        return false;
                     }
                 }
             }
-            if(list.size() == n-1){
-                result=result+1;
-                System.out.println(list);
-                list.clear();
-                initCheckerboard(checkerboard);
+        }
+        return true;
+    }
+    public static boolean process(int[][] checkerboard,int n,int row){
+        //从当前行往下是否能生成结果 true能 false不能
+        if(row == n){
+            return true;
+        }else{
+            for(int i=0;i<n;i++){
+                int[] location={row,i};
+                if(canPut(checkerboard,location,n)){
+                    checkerboard[row][i] = 1;
+                    process(checkerboard,n,row+1);
+                }else{
+                    return false;
+                }
             }
         }
-        return result;
     }
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
@@ -95,6 +69,6 @@ public class nQueensProblem {
         int n= scanner.nextByte();
         int[][] checkerboard=new int[n][n];
         initCheckerboard(checkerboard);
-        System.out.println(process(checkerboard,n));
+        process(checkerboard,n,0);
     }
 }
